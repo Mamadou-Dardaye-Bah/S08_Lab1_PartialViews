729eb7bf462e4d74d6f231304096771b0911d138
Mamadou Dardaye Bah <2393818@cegepmontpetit.ca>
2026-09-18 09:15:52 -0400

    FCT Ajout HuntingLog
    Ajout du Controlleur HuntingLog et des vues Upsert et Index

---
+++ ZombieParty/ZombieParty/Controllers/HuntingLogController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZombieParty.Models;
+using ZombieParty.Models.Data;
+
+namespace ZombieParty.Controllers
+{
+    public class HuntingLogController : Controller
+    {
+        private ZombiePartyDbContext _baseDonnees { get; set; }
+
+        public HuntingLogController(ZombiePartyDbContext baseDonnees)
+        {
+            _baseDonnees = baseDonnees;
+        }
+        public IActionResult Index()
+        {
+            List<HuntingLog> huntingLogs = _baseDonnees.HuntingLogs.ToList();
+            return View(huntingLogs);
+        }
+        public IActionResult Upsert(int? id)
+        {
+            if (id == 0 || id == null)
+                return View(new Weapon());
+            else
+                return View(_baseDonnees.HuntingLogs.Find(id));
+        }
+    }
+}
